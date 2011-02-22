3ebc87bbfe5e0b122ca3089b53deda3515f2247c
Kebang.Huang@gmail.com <Kebang.Huang@gmail.com@e8548c86-b928-10e2-77a1-912313d7f273>
2011-02-22 18:57:02 +0000

    Updated

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -9,80 +9,71 @@ import javax.swing.JPanel;
 
 public class GamePanel extends JPanel
 {
-  private BranchGroup scene, cube;
+	private BranchGroup scene, cube;
 
-  public GamePanel()
-  {
-    setLayout(new BorderLayout());
-//comment that does not change anything
-    GraphicsConfiguration config = 
-	SimpleUniverse.getPreferredConfiguration();
-    Canvas3D canvas3D = new Canvas3D(config);
-    add("Center", canvas3D);
+	public GamePanel()
+	{
+		setLayout(new BorderLayout());
 
-    createSceneGraph();
+		GraphicsConfiguration config = 
+			SimpleUniverse.getPreferredConfiguration();
+		Canvas3D canvas3D = new Canvas3D(config);
+		add("Center", canvas3D);
 
-    SimpleUniverse simpleU = new SimpleUniverse(canvas3D);
+		createSceneGraph();
 
-    // This will move the ViewPlatform back a bit so the
-    // objects in the scene can be viewed.
-    simpleU.getViewingPlatform().setNominalViewingTransform();
+		SimpleUniverse simpleU = new SimpleUniverse(canvas3D);
+		simpleU.getViewingPlatform().setNominalViewingTransform();
+		simpleU.addBranchGraph(scene);
+	}
 
-    simpleU.addBranchGraph(scene);
-  }
+	public void createLifeCube(int rows, int columns, int steps, int lives)
+	{
+		cube.detach();
+    	cube = mouseControl(rows, columns, steps, lives);
+    	scene.addChild(cube);
+	}
 
-  public void createLifeCube(int rows, int columns, int steps, int lives)
-  {
-    // remove the cube create a new one and attach it back
-    cube.detach();
-    cube = mouseControl(rows, columns, steps, lives);
-    scene.addChild(cube);
-  }
+	public void createSceneGraph()
+	{
+		scene = new BranchGroup();
+		scene.setCapability(Group.ALLOW_CHILDREN_WRITE);
+		scene.setCapability(Group.ALLOW_CHILDREN_EXTEND);
 
-  public void createSceneGraph()
-  {
-    scene = new BranchGroup();
-    scene.setCapability(Group.ALLOW_CHILDREN_WRITE);
-    scene.setCapability(Group.ALLOW_CHILDREN_EXTEND);
+		cube = new BranchGroup();
+		cube.setCapability(BranchGroup.ALLOW_DETACH);
+		scene.addChild(cube);
 
-    cube = new BranchGroup();
-    cube.setCapability(BranchGroup.ALLOW_DETACH);
-    scene.addChild(cube);
+		Background backg = new Background(1.0f, 1.0f, 1.0f);
+		backg.setApplicationBounds(new BoundingSphere(new Point3d(), 1000.0));
+		scene.addChild(backg);
 
-    // white background
-    Background backg = new Background(1.0f, 1.0f, 1.0f);
-    backg.setApplicationBounds(new BoundingSphere(new Point3d(), 1000.0));
-    scene.addChild(backg);
+		scene.compile();
+	}
 
-    scene.compile();
-  }
+	public BranchGroup mouseControl(int rows, int columns, int steps, int lives)
+	{
+		cube = new BranchGroup();
+		cube.setCapability(BranchGroup.ALLOW_DETACH);
 
-  public BranchGroup mouseControl(int rows, int columns, int steps, int lives)
-  {
-    cube = new BranchGroup();
-    cube.setCapability(BranchGroup.ALLOW_DETACH);
+		TransformGroup objTransform = new TransformGroup();
+		objTransform.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+		objTransform.setCapability(TransformGroup.ALLOW_TRANSFORM_READ);
 
-    TransformGroup objTransform = new TransformGroup();
-    objTransform.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
-    objTransform.setCapability(TransformGroup.ALLOW_TRANSFORM_READ);
+		objTransform.addChild(new LifeCube(rows, columns, steps, lives).getBG());
+		cube.addChild(objTransform);
 
-    // add cube
-    objTransform.addChild(new LifeCube(rows, columns, steps, lives).getBG());
-    cube.addChild(objTransform);
+		RightMouseRotate myMouseRotate = new RightMouseRotate();
+		myMouseRotate.setTransformGroup(objTransform);
+		myMouseRotate.setSchedulingBounds(new BoundingSphere());
+		cube.addChild(myMouseRotate);
 
-    // mouse left click rotate
-    MouseRotate myMouseRotate = new MouseRotate();
-    myMouseRotate.setTransformGroup(objTransform);
-    myMouseRotate.setSchedulingBounds(new BoundingSphere());
-    cube.addChild(myMouseRotate);
+		MouseZoom myMouseZoom = new MouseZoom();
+		myMouseZoom.setTransformGroup(objTransform);
+		myMouseZoom.setSchedulingBounds(new BoundingSphere());
+		cube.addChild(myMouseZoom);
 
-    // mouse middle button zoom
-    MouseZoom myMouseZoom = new MouseZoom();
-    myMouseZoom.setTransformGroup(objTransform);
-    myMouseZoom.setSchedulingBounds(new BoundingSphere());
-    cube.addChild(myMouseZoom);
-
-    cube.compile();
-    return cube;
-  }
+		cube.compile();
+    	return cube;
+	}
 }
